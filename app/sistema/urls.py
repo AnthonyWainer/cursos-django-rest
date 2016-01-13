59e35cdffcdab5fe278207a6d4741026762eed1b
anthonywainer <guivin95@gmail.com>
2016-01-13 15:59:11 -0500

    formu

--- app/sistema/urls.py
+++ app/sistema/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from .views import index, curso, tipocursos
+from .views import index, cursillo, tipocursos, eliminarcurso, actualizarcurso
 
 urlpatterns = [
     url(r'^$', index, name="inicio"),
     url(r'^addtipo/$', tipocursos),
-    url(r'^addcurso/$', curso),
+    url(r'^addcurso/$', cursillo),
+    url(r'^eliminarcurso/(?P<pk>\d+)/$', eliminarcurso),
+    url(r'^addcurso/(?P<pk>\d+)/$', actualizarcurso),
+
 ]
--- app/sistema/views.py
+++ app/sistema/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect, get_object_or_404
 from .forms import tipocursoForm, cursoForm
 from .models import tipocurso, curso
 # Create your views here.
@@ -24,6 +24,35 @@ def tipocursos(request):
         formulario = tipocursoForm()
     return render(request, 'tipocurso.html', {'formu': formulario, 'tipocurso':tc})
 
-def curso(request):
+def cursillo(request):
+    cu = curso.objects.all()
     formulario = cursoForm()
-    return render(request, 'curso.html', {'formu': formulario })
+
+    if request.method == "POST":
+        f = cursoForm(request.POST)
+        if f.is_valid():
+            f.save()
+        else:
+            formulario = f
+
+    return render(request, 'curso.html', {'formu': formulario, 'cursos':cu })
+
+def actualizarcurso(request, pk):
+    a = get_object_or_404(curso, pk= int(pk))
+    formulario = cursoForm(instance = a)
+
+    if request.method == "POST":
+
+        f = cursoForm(request.POST, instance = a)
+
+        if f.is_valid():
+            f.save()
+            return HttpResponseRedirect("/addcurso")
+        else:
+            formulario = f
+
+    return render(request, 'actualiza_curso.html', {'formu': formulario})
+
+def eliminarcurso(request, pk):
+    u = curso.objects.get(pk=int(pk)).delete()
+    return HttpResponseRedirect("/addcurso")
