7277dedd83da2f3443443dfec79ed0d7a22c73a7
anthonywainer <guivin95@gmail.com>
2016-01-12 23:45:57 -0500

    formmodel

--- app/sistema/forms.py
+++ app/sistema/forms.py
@@ -7,5 +7,31 @@ class tipocursoForm(forms.Form):
 
 class cursoForm(forms.ModelForm):
     class Meta:
+        dc = {'class':'form-control', 'placeholder':'ingresar descripción de curso'}
+        tp = {'class':'form-control'}
+        c = ciclos = (
+                ('I', 'I'), ('II', 'II'), ('III', 'III'), ('IV','IV'),
+                ('V', 'V'), ('VI', 'VI'), ('VII', 'VII'), ('VIII','VIII'),
+                ('IX', 'IX'), ('X', 'X'), ('E', 'E'),
+            )
         model = curso
+        exclude = {'estadocurso'}
         fields = '__all__'
+        labels = {
+            'descripcioncurso' : ('Descripcion del curso'),
+            'tipocurso'        : ('Tipo curso'),
+        }
+        widgets = {
+            'descripcioncurso' : forms.TextInput(attrs=dc),
+            'tipocurso'        : forms.Select(attrs=tp),
+            'creditos'         : forms.NumberInput(attrs=tp),
+            'ciclo'            : forms.Select(attrs=tp, choices=c)
+        }
+        help_texts = {
+            'creditos': ('** No escribir mas de 5 créditos.<br>'),
+        }
+        error_messages = {
+            'creditos': {
+                'max_length': ("Crédito Muy largo."),
+            },
+        }
