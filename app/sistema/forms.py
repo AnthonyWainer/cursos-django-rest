42421bc3b208e1b6210c3ba3c8672c445e7074c9
anthonywainer <guivin95@gmail.com>
2016-01-13 14:53:47 -0500

    asd

--- app/sistema/forms.py
+++ app/sistema/forms.py
@@ -7,28 +7,33 @@ class tipocursoForm(forms.Form):
 
 class cursoForm(forms.ModelForm):
     class Meta:
+        model = curso
+        fields = '__all__'
         dc = {'class':'form-control', 'placeholder':'ingresar descripción de curso'}
         tp = {'class':'form-control'}
+        cr = {'class':'form-control', 'placeholder': 'ingrese número de créditos'}
         c = ciclos = (
                 ('I', 'I'), ('II', 'II'), ('III', 'III'), ('IV','IV'),
                 ('V', 'V'), ('VI', 'VI'), ('VII', 'VII'), ('VIII','VIII'),
                 ('IX', 'IX'), ('X', 'X'), ('E', 'E'),
             )
-        model = curso
+
         exclude = {'estadocurso'}
-        fields = '__all__'
+
         labels = {
             'descripcioncurso' : ('Descripcion del curso'),
             'tipocurso'        : ('Tipo curso'),
+            'creditos'         : ('Créditos'),
         }
         widgets = {
             'descripcioncurso' : forms.TextInput(attrs=dc),
             'tipocurso'        : forms.Select(attrs=tp),
-            'creditos'         : forms.NumberInput(attrs=tp),
+            'creditos'         : forms.NumberInput(attrs=cr),
             'ciclo'            : forms.Select(attrs=tp, choices=c)
         }
         help_texts = {
             'creditos': ('** No escribir mas de 5 créditos.<br>'),
+            'ciclo'   : ('** Seleccione un ciclo.<br>'),
         }
         error_messages = {
             'creditos': {
